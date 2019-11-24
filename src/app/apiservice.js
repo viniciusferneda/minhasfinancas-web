b01e1097edca7e45bfb540eeafef9e0d396dadbc
viniciusferneda <vinicius.ferneda@gmail.com>
2019-11-24 17:28:37 -0300

    ajustando a url para o heroku

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: 'https://minhasfinancas-vferneda.herokuapp.com/'
 })
 
 class ApiService {
